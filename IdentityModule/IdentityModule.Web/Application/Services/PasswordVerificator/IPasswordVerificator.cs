67434c62eed7f0be170e32c6364a404cdf2d5186
danielzharikov <zxczxczxcxzc>
2022-12-01 02:57:11 +0300

    Removed extra Identity Module

--- IdentityModule/IdentityModule.Web/Application/Services/PasswordVerificator/IPasswordVerificator.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace IdentityModule.Web.Application.Services.PasswordVerificator;
-
-public interface IPasswordVerificator
-{
-    Task<PasswordVerificationResult> CheckOneTimePassword(string phoneNumber, string password, bool once = true);
-}
